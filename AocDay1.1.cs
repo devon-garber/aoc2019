61e3d52ef1d0627b826e0b84f591febd120b4d98
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2021-04-30 14:12:07 -0700

    Add files via upload

---
+++ AocDay1.1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadLines("input1.1.txt").ToArray();
+            int total = 0;
+            foreach (int num in input.Select(x => Int32.Parse(x)))
+            {
+                int div = num / 3;
+                total += div - 2;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}

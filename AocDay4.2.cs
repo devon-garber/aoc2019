5f87ad125463d0a3f1e28af7c34f90045a56b316
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2021-05-03 14:33:33 -0700

    Add files via upload

---
+++ AocDay4.2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadLines("input4.1.txt").ToArray();
+            int matchCount = 0;
+            if (input.Length == 1)
+            {
+                string[] range = input.First().Split('-');
+                if (range.Length == 2)
+                {
+                    for (int i = Int32.Parse(range.First()); i <= Int32.Parse(range[1]); ++i)
+                    {
+                        bool twoAdj = false;
+                        bool neverDecrease = true;
+                        string s = i.ToString();
+                        for (int j = 0; j < s.Length - 1; ++j)
+                        {
+                            if (s[j] > s[j + 1])
+                            {
+                                neverDecrease = false;
+                            }
+                        }
+
+                        if (neverDecrease)
+                        {
+                            for (int j = 0; j < s.Length - 1; ++j)
+                            {
+                                int numberCount = s.Count(x => x == s[j]);
+                                if (numberCount == 2)
+                                {
+                                    twoAdj = true;
+                                }
+                            }
+                        }
+
+                        if (twoAdj && neverDecrease)
+                        {
+                            matchCount++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(matchCount);
+        }
+    }
+}
